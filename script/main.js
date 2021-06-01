c6b9e4cb8f503ae4b1df4c7c8d2a4301fe5c4665
AndreyBuslaev <AndreyBuslaev@users.noreply.github.com>
2021-06-01 20:34:45 +0300

    add save

--- script/main.js
+++ script/main.js
@@ -6,29 +6,36 @@ function handleFileSelect(evt) {
 
       if (!f.type.match('image.*')) {
         continue;
-      }
+      };
 
       let reader = new FileReader();
 
       reader.onload = (function(theFile) {
         return function(e) {
           let div = document.createElement('div');
-          div.className = 'img-block'
-        //   let button = document.createElement('button');
-        //   button.className = 'btn';
-        //   button.innerHTML = 'Delete';
-        div.innerHTML = ['<img class="thumb" src="', e.target.result,
-                            '" title="', escape(theFile.name), '"/>' + '<p class="thumb-text">',theFile.name,'</p>' + '<button class = "btn">Delete</button>'].join('');
-            document.getElementById('list').insertBefore(div, null)           
-        //   document.getElementById('list').insertBefore(button, null);
+          div.className = 'img-block';
+          div.innerHTML = ['<img class="thumb" src="', e.target.result,'" title="', escape(theFile.name), '"/>' + '<p class="thumb-text">',theFile.name,'</p>' + '<button class = "btn" onclick ="deleteData()">Delete</button>'].join('');
+          document.getElementById('list').insertBefore(div, null);        
         };
       })(f);
 
       reader.readAsDataURL(f);
-    }
-  }
+    };
+  };
 
   document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
+  function saveData(el) {
+    localStorage.setItem(el.id, JSON.stringify(el.value));
+  };
+  
+  function getData() {
+    let inp = document.getElementById("files");
+    inp.value = JSON.parse(localStorage.getItem('files')) || "";
+  };
+
+  function deleteData(el){
+      localStorage.removeItem(el.id, JSON.stringify(el.value))
+  }
 
   

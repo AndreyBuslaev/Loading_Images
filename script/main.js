e262626d3ba6559eb88592651f1b9d443f16c46f
AndreyBuslaev <AndreyBuslaev@users.noreply.github.com>
2021-06-01 21:25:55 +0300

    fix js

--- script/main.js
+++ script/main.js
@@ -1,41 +1,65 @@
+function addImage(fileName, imageSrc) {
+    let div = document.createElement('div');
+    div.className = 'img-block';
+
+    const image = new Image();
+    image.src = imageSrc;
+    image.title = fileName;
+    image.classList.add('thumb');
+
+    const para = document.createElement('p');
+    para.classList.add('thumb-text');
+    para.innerHTML = fileName;
+
+    const button = document.createElement('button');
+    button.classList.add('btn');
+    button.addEventListener('click', () => deleteData(div, fileName));
+    button.innerHTML = 'Delete';
+
+    div.appendChild(image);
+    div.appendChild(para);
+    div.appendChild(button);
+    document.getElementById('list').insertBefore(div, null);
+}
 
 function handleFileSelect(evt) {
     let files = evt.target.files;
 
-    for (let i = 0, f; f = files[i]; i++) {
+    for (let f of files) {
+        if (!f.type.match('image.*')) {
+            continue;
+        }
 
-      if (!f.type.match('image.*')) {
-        continue;
-      };
+        let reader = new FileReader();
 
-      let reader = new FileReader();
+        reader.onload = (function (theFile) {
+            return function (e) {
+                const imageSrc = e.target.result;
+                const fileName = theFile.name;
+                addImage(fileName, imageSrc);
+                saveFile(fileName, imageSrc);
+            };
+        })(f);
 
-      reader.onload = (function(theFile) {
-        return function(e) {
-          let div = document.createElement('div');
-          div.className = 'img-block';
-          div.innerHTML = ['<img class="thumb" src="', e.target.result,'" title="', escape(theFile.name), '"/>' + '<p class="thumb-text">',theFile.name,'</p>' + '<button class = "btn" onclick ="deleteData()">Delete</button>'].join('');
-          document.getElementById('list').insertBefore(div, null);        
-        };
-      })(f);
+        reader.readAsDataURL(f);
+    }
+}
 
-      reader.readAsDataURL(f);
-    };
-  };
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
-  document.getElementById('files').addEventListener('change', handleFileSelect, false);
+function saveFile(fileName, src) {
+    localStorage.setItem(fileName, src);
+}
 
-  function saveData(el) {
-    localStorage.setItem(el.id, JSON.stringify(el.value));
-  };
-  
-  function getData() {
-    let inp = document.getElementById("files");
-    inp.value = JSON.parse(localStorage.getItem('files')) || "";
-  };
+function loadFiles() {
+    for (const fileName of Object.keys(localStorage)) {
+        addImage(fileName, localStorage.getItem(fileName));
+    }
+}
 
-  function deleteData(el){
-      localStorage.removeItem(el.id, JSON.stringify(el.value))
-  }
+function deleteData(div, fileName) {
+    div.parentElement.removeChild(div);
+    localStorage.removeItem(fileName);
+}
 
-  
+window.addEventListener('load', loadFiles);
